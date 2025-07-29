5ccae6c33dca8db5f0c6206a0932ca564f1772e2
Abinash Dash <abinashdashvssut@gmail.com>
2025-07-29 07:26:39 +0530

    Created SinglyNode

--- datastructures/src/com/dash/abinash/linkedlist/nodes/SinglyNode.java
+++ datastructures/src/com/dash/abinash/linkedlist/nodes/SinglyNode.java
@@ -4,4 +4,21 @@ package com.dash.abinash.linkedlist.nodes;
  * A Singly Linked List node
  */
 public class SinglyNode<T> {
+
+    // SinglyNode data
+    T t;
+
+    // Address of the next node
+    SinglyNode<T> next;
+
+    /*
+       SinglyNode constructor contains following
+          - Node data (Primitive or Wrapper)
+          - Address of the next node. For an individual isolated node, the address to the next node
+            is always `Null` until the link is established to another node.
+     */
+    SinglyNode(T t) {
+        this.t = t;
+        next = null;
+    }
 }
