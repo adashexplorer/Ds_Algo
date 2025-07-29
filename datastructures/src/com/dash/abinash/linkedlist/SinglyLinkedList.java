8cb111a78e0baa070a1aa17a74e87d33347f2aff
Abinash Dash <abinashdashvssut@gmail.com>
2025-07-29 08:08:40 +0530

    Added basic operations in a Singly LinkedList

--- datastructures/src/com/dash/abinash/linkedlist/SinglyLinkedList.java
+++ datastructures/src/com/dash/abinash/linkedlist/SinglyLinkedList.java
@@ -3,11 +3,77 @@ package com.dash.abinash.linkedlist;
 import com.dash.abinash.linkedlist.nodes.SinglyNode;
 
 /**
- * A Singly Linked List implementation
+ * A Singly Linked List implementation which contains various functionalities (basic, intermediate & advanced)
  * @author abinash
  * @param <T>
  */
 public class SinglyLinkedList<T> {
 
+
+    /**
+     *    CREATE OPERATIONS
+     *       - Add node to the front of the Singly LinkedList
+     *       - Add node to the end of the Singly LinkedList
+     *       - Add node to the nth node (at a specific index) of the Singly LinkedList
+     *       - Add node to the middle of the Singly LinkedList
+     *       - Add a node after a specific node in a Singly LinkedList
+     *       - Add a node before a specific node in a Singly LinkedList (DLL / CDLL only)
+     */
+
+
+
+    /**
+     *    DELETE OPERATIONS
+     *        - Delete node from the start of the Singly LinkedList
+     *        - Delete node from the end of the Singly LinkedList
+     *        - Delete node from nth node (at a specific index) of the Singly LinkedList
+     *        - Delete node from the middle of the Singly LinkedList
+     *        - Delete node after a specific node in a Singly LinkedList
+     *        - Delete node before a specific node in a Singly LinkedList (DLL / CDLL only)
+     *        - Delete entire Singly LinkedList
+     */
+
+
+    /**
+     *    UPDATE OPERATIONS
+     *        - Update node at the front of the Singly LinkedList
+     *        - Update node at the end of the Singly LinkedList
+     *        - Update node at a specific index
+     *        - Update a specific node with a new node
+     *
+     */
+
+
+    /**
+     *    READ OPERATIONS
+     *        - Traverse all the nodes of the Singly LinkedList from forward direction
+     *        - Traverse all the nodes of the Singly LinkedList from backward direction (DLL / CDLL)
+     *        - Print all values from head to tail of a Singly LinkedList
+     */
+
+
+    /**
+     *    SEARCH OPERATIONS
+     *        - Search en element in a Singly LinkedList
+     *        - Search an element in a Singly LinkedList at a specific index / Find nth node from end of a Singly LinkedList
+     *        - Find middle node in a Singly LinkedList
+     *        - Find index of a specific node in a Singly LinkedList
+     */
+
+
+    /**
+     *    UTILITY OPERATIONS
+     *        - Get length of the Singly LinkedList
+     *        - Check if Singly LinkedList is empty or not
+     *        - Reverse the Singly LinkedList
+     *        - Detect a loop in Singly LinkedList
+     *        - Remove duplicates from a Singly LinkedList
+     *        - Sort the Singly LinkedList (Ascending & Descending)
+     *        - Compare two Singly LinkedLists
+     *        - Convert Singly LinkedList to array
+     *        - Merge two Singly LinkedLists
+     *        - Clone a Singly LinkedList to another List
+     *        - Rotate a Singly LinkedList from left to right OR vice-versa
+     */
 
 }
